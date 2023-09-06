c0ec3f83845aafeb3556802a4e418077c5803eaf
wwwwwls <130200742+wwwwwls@users.noreply.github.com>
2023-09-06 21:41:11 +0800

    Create 785-快排.cpp

---
+++ 基础算法/785-快排.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+const int N = 100010;
+int a[N];
+int n;
+void quick_sort(int a[], int l, int r)
+{
+	if(l >= r) return;
+	
+	int i = l-1, j = r+1;
+	int x = a[(i + j) >> 1];
+	while(i < j)
+	{
+		do i++ ;while(a[i] < x);
+		do j-- ;while(a[j] > x);
+		if(i < j) swap(a[i], a[j]);
+	}
+	quick_sort(a, l, j);
+	quick_sort(a, j+ 1, r);
+}
+int main()
+{
+	scanf("%d", &n);
+	for(int i= 0; i<n; i++)
+		scanf("%d", &a[i]);
+	quick_sort(a, 0, n -1);
+	for(int i = 0; i< n; i++)
+		printf("%d ", a[i]);
+	
+	return 0;
+}
